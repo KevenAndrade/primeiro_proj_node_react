34dbe093aff14de9f260d4d9a41746d24784c214
Keven Andrade <5674263-Keven_@users.noreply.gitlab.com>
2021-02-18 15:00:29 -0100

    Cap 4 ep 6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header";
 
 function App(){
+    const [projects,setprojects] = useState(['Desenvolvimento de backend', 'Desenvolvimento Mobile']);
+
+    // useState ta retorna um array k 2 possison
+    // 1. valor de variavel work 
+    // 2. funson pa atualisa variavel
+
+    function addproject() {
+
+        setprojects([...projects,`Novo Projeto at: ${Date.now()}`]);
+
+        console.log(projects);
+    }
+
     return (
         <>
-        <Header />
-        <Header />
+        <Header title="Home"/>
+
+        <ul>
+            {projects.map(project =><li key={project}>{project}</li>)}
+        </ul>
+
+        <button type="button" onClick={addproject} > Add novo projeto </button>
         </>
     );
 }
